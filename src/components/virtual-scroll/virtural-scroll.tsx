2f4aad27e08e5cb492e798d50a148feb22bdb889
FLJ <617229004@qq.com>
2023-05-04 09:36:41 +0800

    save

---
+++ src/components/virtual-scroll/virtural-scroll.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './index.less'
+
+const index = function () {
+    return (
+        <div className='list'>
+            12312
+        </div>
+    )
+};
+export default index;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, RouterProvider, createBrowserRouter} from "react-router-dom";
+import VirturalScroll from "./components/virtual-scroll/virtural-scroll";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>,
+    },
+    {
+        path: '/virtual-scroll',
+        element: <VirturalScroll height={300} fuzzyItemHeight={30} listData={[{key: 1, text: "1"},
+            {key: 1, text: "2"},
+            {key: 1, text: "3"},
+            {key: 1, text: "4"},
+            {key: 1, text: "5"},
+            {key: 1, text: "6"},
+            {key: 1, text: "7"},
+            {key: 1, text: "8"},
+            {key: 1, text: "9"},
+            {key: 1, text: "10"}]}/>
+    }
+]);
+ReactDOM.render(
+    <React.StrictMode>
+        <RouterProvider router={router}/>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
